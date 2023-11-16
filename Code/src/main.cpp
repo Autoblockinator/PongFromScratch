414928b2774eabbe66d34b0bb0f25e3b97417b13
Alison Janvier <autoblockinator@gmail.com>
2023-11-16 19:03:12 +0000

    commit

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "Pong" };
+    auto window = sf::RenderWindow{{1920u, 1080u}, "Pong"};
     window.setFramerateLimit(144);
 
     while (window.isOpen())
@@ -14,8 +14,14 @@ int main()
                 window.close();
             }
         }
-
+        
         window.clear();
+
+        auto rect = sf::RectangleShape({20, 100});
+        rect.setPosition({50, 50});
+        rect.setFillColor(sf::Color::White);
+        window.draw(rect);
+
         window.display();
     }
 
