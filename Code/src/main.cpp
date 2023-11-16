dbb29a788fe9ee45e2ae19302738d1104b30c9b1
Alison Janvier <autoblockinator@gmail.com>
2023-11-16 21:50:12 +0000

    commit

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -3,12 +3,6 @@
 
 #include "node.cpp"
 
-void is_node_root(Node* node)
-{
-    if (node->parent == nullptr) { std::cout << "Node is root." << std::endl; }
-    else { std::cout << "Node is not root." << std::endl; }
-}
-
 int main()
 {
     auto window = sf::RenderWindow{{1920, 1080}, "Pong"};
@@ -17,21 +11,13 @@ int main()
     Node root;
     Node child;
 
-    root.parent = nullptr;
-    root.add_child(&child);
-
-    is_node_root(&root);
-    is_node_root(&child);
-
-    while (window.isOpen())
+    while (true)
     {
         for (auto event = sf::Event{}; window.pollEvent(event);)
         {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
+            if (event.type == sf::Event::Closed) { window.close(); }
         }
+        if (!window.isOpen()) { break; }
 
         window.clear(sf::Color::Black);
 
--- Code/src/node.cpp
+++ Code/src/node.cpp
@@ -1,30 +1,37 @@
 #pragma once
-#include <iostream>
 #include <vector>
 
 class Node
 {
 public:
-    Node* parent;
-    std::vector<Node*> children;
-
     Node()
     {
         parent = nullptr;
         children = std::vector<Node*>();
-        std::cout << "Constructor Called" << std::endl;
     }
 
-    Node* add_child(Node* new_child)
+    Node* getParent() { return parent; }
+
+    Node* setParent(Node* new_parent)
+    {
+        parent = new_parent;
+        return new_parent;
+    }
+
+    Node* addChild(Node* new_child)
     {
         if (new_child == nullptr) { return nullptr; }
-
         children.push_back(new_child);
+        new_child->parent = this;
         return new_child;
     }
 
-    Node* find_node(const char* path)
+    Node* findNode(const char* path)
     {
         return nullptr;
     }
+
+protected:
+    Node* parent;
+    std::vector<Node*> children;
 };
