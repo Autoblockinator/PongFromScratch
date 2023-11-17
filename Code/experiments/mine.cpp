c361137091b3edceaf31c6e17dea367bba6f09c0
Alison Janvier <autoblockinator@gmail.com>
2023-11-17 22:36:31 +0000

    commit

--- Code/experiments/mine.cpp
+++ Code/experiments/mine.cpp
@@ -1,23 +1,68 @@
 #include <iostream>
-#define log(text) std::cout << text << std::endl
+#define log(text) std::cout << text
+#define logLine(text) log(text) << std::endl
+
 #include <vector>
 #define Vector std::vector
+
 #include <string>
 #define String std::string
 
 template <typename T>
 class CustomSmartPointer {
 public:
-    CustomSmartPointer(): ptr(nullptr) borrowers() {}
+    #ifndef DEFAULT
+    #define DEFAULT ptr(nullptr), borrowers(Vector<CustomSmartPointer<T>*>())
+    #endif
+
+    CustomSmartPointer() { constructDefault(); }
+
+    explicit CustomSmartPointer(T* ptr) { constructDefault(); this->ptr = ptr; }
+
+    CustomSmartPointer(const CustomSmartPointer<T>& owner) { constructDefault(); borrow(owner); }
+
+    #ifdef DEFAULT
+    #undef DEFAULT
+    #endif
+
+    CustomSmartPointer& operator=(const CustomSmartPointer<T>& owner) { borrow(owner); return *this; }
+
+    void borrow(const CustomSmartPointer<T>& owner) { ptr = owner.ptr; owner.borrowers.push_back(this); }
+
+    ~CustomSmartPointer() {
+
+    }
+
+    T* operator->() const { return ptr; }
+
+    T& get() const { return *ptr; }
+
+    CustomSmartPointer<T>& getOwner() {
+        if (typeid(*ptr) == typeid(CustomSmartPointer<T>)) { return *ptr; }
+        else { return nullptr; }
+    }
 
 protected:
     T* ptr;
-    Vector<CustomSmartPointer*> borrowers;
+    Vector<CustomSmartPointer<T>*> borrowers;
+
+    void constructDefault() { ptr = nullptr; borrowers = Vector<CustomSmartPointer<T>*>(); }
+};
+
+class TestClass {
+public:
+    String A;
+    String B;
+
+    TestClass(String a, String b): A(a), B(b) {}
 };
 
 int main() {
-    auto var = 69;
+    TestClass var{"Hello ", "World! "};
+    logLine("var: " << var.A << var.B);
 
-    log("");
+    CustomSmartPointer csp{&var};
+    logLine("csp->: " << csp->A << csp->B);
+
     return 0;
 }
