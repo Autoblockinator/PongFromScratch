ac23ab7dbf8feb4b9d665a7625c8607f3dbe0584
Alison Janvier <autoblockinator@gmail.com>
2023-11-18 21:52:01 +0000

    commit

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,49 +1,33 @@
 #include <SFML/Graphics.hpp>
 #include <utils/all.h>
-#include <node.h>
-#include <window.h>
 
-class Root: Node {
-public:
-    Root(): Node()
+int main()
+{
+    auto window = sf::RenderWindow{{1920, 1080}, "Pong"};
+    window.setFramerateLimit(144);
+
+    while (true)
     {
-        logLine("Root Init");
-    }
-};
+        for (sf::Event event; window.pollEvent(event);)
+        {
+            if (event.type == sf::Event::Closed) { window.close(); }
+        }
+        if (!window.isOpen()) { break; }
 
-int main() {
-    Root root;
+        window.clear(sf::Color::Black);
+
+        auto rect = sf::RectangleShape({20, 100});
+        rect.setPosition({50, 50});
+        rect.setFillColor(sf::Color::White);
+        window.draw(rect);
+
+        auto rect2 = sf::RectangleShape({20,100});
+        rect2.setPosition({static_cast<float>(window.getSize().x-(50+20)), 50});
+        rect2.setFillColor(sf::Color::White);
+        window.draw(rect2);
+
+        window.display();
+    }
 
     return 0;
 }
-
-// int main()
-// {
-//     auto window = sf::RenderWindow{{1920, 1080}, "Pong"};
-//     window.setFramerateLimit(144);
-
-//     while (true)
-//     {
-//         for (sf::Event event; window.pollEvent(event);)
-//         {
-//             if (event.type == sf::Event::Closed) { window.close(); }
-//         }
-//         if (!window.isOpen()) { break; }
-
-//         window.clear(sf::Color::Black);
-
-//         auto rect = sf::RectangleShape({20, 100});
-//         rect.setPosition({50, 50});
-//         rect.setFillColor(sf::Color::White);
-//         window.draw(rect);
-
-//         auto rect2 = sf::RectangleShape({20,100});
-//         rect2.setPosition({static_cast<float>(window.getSize().x-(50+20)), 50});
-//         rect2.setFillColor(sf::Color::White);
-//         window.draw(rect2);
-
-//         window.display();
-//     }
-
-//     return 0;
-// }
--- Code/src/window.cpp
+++
@@ -1,6 +0,0 @@
-#include <utils/all.h>
-#include <node.h>
-
-class Window: Node {
-
-};
